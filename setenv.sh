b591b29c6584f36c7204d2981f70f2efa71171ab
Arpit Mohan <arpit@appsmith.com>
2020-12-12 21:32:52 +0530

    Use single eq

--- setenv.sh
+++ setenv.sh
@@ -4,6 +4,6 @@ REACT_APP_ENVIRONMENT="DEVELOPMENT";
 echo "Github Ref: ${GITHUB_REF}";
 echo "Github Ref: $GITHUB_REF";
 echo ::set-output name=REACT_APP_ENVIRONMENT::PRODUCTION
-if [[ "$GITHUB_REF" == "/refs/heads/main" ]]; then
+if [ "${GITHUB_REF}" = "/refs/heads/main" ]; then
     echo "In the main"
 fi

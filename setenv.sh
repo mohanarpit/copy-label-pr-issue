cb084baa507134530cb9775142f810b75b986211
Arpit Mohan <arpit@appsmith.com>
2020-12-12 21:19:09 +0530

    Moving to scrpit

---
+++ setenv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+REACT_APP_ENVIRONMENT="DEVELOPMENT";
+echo "Github Ref: ${GITHUB_REF}";
+echo "Github Ref: $GITHUB_REF";
+echo ::set-output name=REACT_APP_ENVIRONMENT::PRODUCTION
+if [ "$GITHUB_REF" = '/refs/heads/main' ]; then
+    echo "In the main"
+fi

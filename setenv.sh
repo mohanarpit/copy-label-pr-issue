f3f02bcd0c7132a3f6c2ea7900c1f001eebd21e7
Arpit Mohan <arpit@appsmith.com>
2020-12-12 21:35:03 +0530

    Using 3rd variable

--- setenv.sh
+++ setenv.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 REACT_APP_ENVIRONMENT="DEVELOPMENT";
+GITHUB_BRANCH_REFERENCE=${GITHUB_REF};
 echo "Github Ref: ${GITHUB_REF}";
-echo "Github Ref: $GITHUB_REF";
+echo "Github Branch Ref: $GITHUB_BRANCH_REFERENCE";
 echo ::set-output name=REACT_APP_ENVIRONMENT::PRODUCTION
-if [ "${GITHUB_REF}" = "/refs/heads/main" ]; then
+if [ "$GITHUB_BRANCH_REFERENCE" = "/refs/heads/main" ]; then
     echo "In the main"
 fi
